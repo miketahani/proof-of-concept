d13f99ddfdbdc7532fc21c499a220a7b4ec14952
Mike Tahani <me@miketahani.com>
2020-04-10 20:17:49 -0700

    remove dependency on global evented localStore; minor cleanup

--- localstorage-events-namespaces/index.js
+++ localstorage-events-namespaces/index.js
@@ -58,31 +58,33 @@ function eventedStorage (storage = localStorage) {
 
 const localStore = eventedStorage(localStorage)
 
-const getKeysForNamespace = namespace => {
-  return Object.keys(localStore).filter(key => key.startsWith(namespace))
-}
-
 // localStore is independent from useLocalStorageNS, which implements
 // namespacing and serves as a native api abstraction
-export function useLocalStorageNS (namespace, store = localStorage) {
+export function useLocalStorageNS (namespace, storage = localStorage) {
   const ns = useCallback((key = '') => `${namespace}/${key}`, [namespace])
+
+  const getKeys = useCallback(() => {
+    const _namespace = ns()
+    return Object.keys(storage).filter(key => key.startsWith(_namespace))
+  }, [ns, storage])
 
   const clear = useCallback(() => {
     // only clear our namespace
-    getKeysForNamespace(ns()).forEach(store.removeItem)
-  }, [ns])
+    getKeys().forEach(storage.removeItem)
+  }, [getKeys, storage])
 
   const getNS = useCallback(() => {
     const _namespace = ns()
-    return getKeysForNamespace(_namespace).map(key =>
-      [key.replace(_namespace, ''), store.getItem(key)]
-    )
-  }, [ns])
+    return getKeys().map(key => [
+      key.replace(_namespace, ''),
+      storage.getItem(key)
+    ])
+  }, [ns, getKeys, storage])
 
   // need a proxy for opaquely namespacing keys on `{get|set|remove}Item`
   // FIXME cache the proxy in a ref so we're not constantly creating new
   //       proxies on rerenders?
-  return new Proxy(store, {
+  return new Proxy(storage, {
     set (ls, key, value) {
       ls.setItem(ns(key), value)
     },
