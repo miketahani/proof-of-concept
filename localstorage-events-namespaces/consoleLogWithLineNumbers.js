4241dc710b2c86c2fca66ea0411ea27b12f45f41
Mike Tahani <me@miketahani.com>
2020-04-27 19:02:39 -0700

    oh

--- localstorage-events-namespaces/consoleLogWithLineNumbers.js
+++ localstorage-events-namespaces/consoleLogWithLineNumbers.js
@@ -1,19 +1,9 @@
-// add line numbers to console.log, using proxies to wrap native browser APIs.
+// add line numbers to console.log
 // unfortunately, this breaks console.log css styling via `%c`.
 ;(function () {
+  const _consoleLog = globalThis.console.log.bind(globalThis.console)
   let lineNum = 0
-
-  // have to use a proxy bc using `Object.defineProperty(console, 'log', ...)` will
-  // cause an infinite call loop
-  const proxy = new Proxy(globalThis.console, {
-    get (_console, prop) {
-      if (prop === 'log') {
-        return (...args) => _console.log.call(_console, ++lineNum, ...args)
-      }
-      const p = _console[prop]
-      return typeof(p) === 'function' ? p.bind(_console) : p
-    }
+  Object.defineProperty(globalThis.console, 'log', {
+    get: () => (...args) => _consoleLog(++lineNum, ...args)
   })
-
-  Object.defineProperty(globalThis, 'console', { get: () => proxy })
 })();
